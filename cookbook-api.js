5650a71fc8c5b7b3570772051b58b1e6c7ec0daf
Tyler Whitehurst <twwhitehurst@gmail.com>
2023-11-21 23:56:40 +0000

    stash api stuff

---
+++ cookbook-api.js
@@ -0,0 +1,106 @@
+const functions = require("@google-cloud/functions-framework");
+const fs = require("fs");
+
+// lib
+const { Storage: Storage } = require("@google-cloud/storage"),
+  os = require("os"),
+  path = require("path"),
+  storage = new Storage();
+function tempPath() {
+  return path.join(os.tmpdir(), "temp-" + Date.now() + ".json");
+}
+function enableCors(e, t) {
+  return (
+    t.set("Access-Control-Allow-Origin", "*"),
+    "OPTIONS" === e.method &&
+      (t.set("Access-Control-Allow-Methods", "GET, PUT, POST"),
+      t.set("Access-Control-Allow-Headers", "Content-Type"),
+      t.set("Access-Control-Max-Age", "3600"),
+      t.status(204).send(""),
+      !0)
+  );
+}
+async function writeJsonToFile(fileName, jsonObject) {
+  return new Promise((resolve, reject) => {
+    const jsonString = JSON.stringify(jsonObject);
+    const filename = fileName;
+    fs.writeFile(filename, jsonString, 'utf8', (err) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(filename);
+      }
+    });
+  });
+}
+async function getFile(e, t) {
+  const o = tempPath();
+  try {
+    await storage.bucket(e).file(t).download({ destination: o });
+  } catch (e) {
+    await writeJsonToFile(o, {"err": "not found"});
+  } finally {
+  }
+  return o;
+}
+function getIdFromPath(path) {
+  const id = path.split("/")[2].split("?")[0];
+  return id;
+}
+// end lib
+
+const bucketName = "gs://whitehurst-cook-book";
+const getRecipePath = (recipeId) => `recipes/${recipeId}.json`;
+
+functions.http("recipes", (req, res) => {
+  if (enableCors(req, res)) return;
+  if (req.method === "GET") {
+    const path = req.path;
+    if (path.startsWith("/recipe")) {
+      const id = getIdFromPath(path);
+      recipePath = getRecipePath(id);
+      try {
+        getFile(bucketName, recipePath)
+          .then((temp) => fs.readFileSync(temp, "utf8"))
+          .then((file) => res.send(file));
+      } catch (e) {
+        res.send("Not Found");
+      }
+    } else if (path.startsWith("/preview")) {
+      res.send("preview apis");
+    } else {
+      res.send("404");
+    }
+  } else if (req.method === "POST") {
+    // todo create recipe
+  } else if (req.method === "PUT") {
+    // todo update recipe
+  }
+});
+
+// function updateRecipe(recipeId, recipeJson, res) {
+//   writeJsonToTempFile(recipeJson)
+//     .then(filename =>
+//       uploadFile(filename, recipeId).then(_ => filename)
+//     )
+//     .then(filename =>
+//         fs.unlink(filename, () => res.send(recipeId))
+//     )
+// }
+
+//
+// async function uploadFile(fileName) {
+//   try {
+//     await storage.bucket(bucketName).upload(fileName, {
+//       gzip: true,
+//       destination: "recipes",
+//       metadata: {
+//         cacheControl: 'no-cache',
+//       },
+//     });
+//
+//     console.log(`${fileName} uploaded to ${bucketName}.`);
+//   } catch (error) {
+//     console.error('ERROR:', error);
+//   }
+// }
